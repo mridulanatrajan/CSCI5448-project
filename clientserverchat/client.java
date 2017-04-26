b0432035ee8af8c3d30e3b97104a9b4177b59f2d
Rajarshi Basak <rajarshi.basak@colorado.edu>
2017-04-26 17:45:32 -0600

    Add files via upload

---
+++ clientserverchat/client.java
@@ -0,0 +1,26 @@
+package clientserverchat;
+
+import java.io.*;
+import java.net.*;
+
+public class client {
+	public static void main(String[] args) {
+		try{
+			Socket s = new Socket("127.0.0.1", 1299);
+			DataInputStream din = new DataInputStream(s.getInputStream());
+			DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+			
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			String msgin = "", msgout = "";
+			
+			while (!msgin.equals("end")) {
+				msgout = br.readLine();
+				dout.writeUTF(msgout);
+				msgin = din.readUTF();
+				System.out.println(msgin);
+			}
+		}catch(Exception e) {
+			
+		}
+	}
+}
