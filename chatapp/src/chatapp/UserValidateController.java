aa0753326f79e8d917e936e252f0d44ab8396c63
Rajarshi Basak <rajarshi.basak@colorado.edu>
2017-05-01 22:37:10 -0600

    Update UserValidateController.java

--- chatapp/src/chatapp/UserValidateController.java
+++ chatapp/src/chatapp/UserValidateController.java
@@ -7,20 +7,20 @@ import java.sql.SQLException;
 import com.mysql.jdbc.Connection;
 import com.mysql.jdbc.PreparedStatement;
 
-public abstract class UserValidateController {
+public abstract class userValidateController {
 	
 	boolean valid;
 	String uname;
 	String upass;
 	User per;
 	
-	public UserValidateController(String u,User uu) throws ClassNotFoundException, SQLException
+	public userValidateController(String u,User uu) throws ClassNotFoundException, SQLException
 	{
 	 uname=u;
 	 per=uu;
 	 valid=connecttoDB();	
 	}
-	public UserValidateController(String u, String p,User uu) throws ClassNotFoundException, SQLException
+	public userValidateController(String u, String p,User uu) throws ClassNotFoundException, SQLException
 	{
 	 uname=u;
 	 upass=p;
